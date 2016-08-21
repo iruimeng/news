d166e9ae02e0d07ccb067b97aede1dbfbe6e1f9e
zhaozhi <zhaozhi@xycentury.com>
2016-08-21 17:56:19 +0800

    下载头图

--- mongodb_to_mysql.py
+++ mongodb_to_mysql.py
@@ -84,6 +84,7 @@ def readCrawledArticles(mongoClient, mysqlConn):
         for doc in result_db[collection_name].find():
             result = json.loads(doc["result"])
             content_imgs = result.get("content_imgs", [])
+            main_img = result.get("img", "")
             title = result["title"]
             dateStr = result.get("date", "")
             dt = parseDateStr(dateStr)
@@ -106,14 +107,18 @@ def readCrawledArticles(mongoClient, mysqlConn):
             }
             article_id = saveToMysql(mysqlConn, data)
             print article_id
-            if len(content_imgs) > 0 and article_id != None:
-                saveArticleImages(mysqlConn, content_imgs, img_processor, article_id, doc["taskid"])
+            if (len(content_imgs) > 0 or main_img != "") and article_id != None:
+                saveArticleImages(mysqlConn, content_imgs, main_img, img_processor, article_id, doc["taskid"])
 
-def saveArticleImages(mysqlConn, img_urls, img_processor, article_id, task_id):
+def saveArticleImages(mysqlConn, img_urls, main_img, img_processor, article_id, task_id):
     sql = "insert into article_images (article_id, seq, image_path, `type`, create_time, update_time) values (%(article_id)s, %(seq)s, %(image_path)s, %(type)s, %(create_time)s, %(update_time)s) on duplicate key update image_path=values(image_path), update_time=values(update_time)"
 
+    start_seq = 1
+    if main_img != "" and main_img != None:
+        start_seq = 0
+        img_urls[0:0] = [main_img]
     for i in xrange(len(img_urls)):
-        seq = i+1
+        seq = i + start_seq
         ret = img_processor.save(img_urls[i], "%s-%d" % (task_id, seq))
         for key, val in ret.iteritems():
             time_str = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
@@ -130,7 +135,7 @@ def saveArticleImages(mysqlConn, img_urls, img_processor, article_id, task_id):
 
 if __name__ == "__main__":
     mongoClient = pymongo.MongoClient("mongodb://47.88.194.127:27017/", connect=False)
-    mysqlConn = mysql.connector.connect(user="root", password="root2my", host="47.88.194.127", database="news", charset="utf8")
+    mysqlConn = mysql.connector.connect(user="zhaozhi", password="zzhao", host="47.88.194.127", database="news_test", charset="utf8")
 
     if mongoClient == None:
         print "connect to mongodb error"
